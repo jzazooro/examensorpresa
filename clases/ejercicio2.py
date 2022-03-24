24e83b79ed8b1e0fc871a7b1af5a291a8d14eac3
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:58:09 +0100

    correccion

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -1,14 +1,17 @@
 class animal(self, tipo):
     def __init__(self, tipo):
         self.tipo=tipo
+
 class mamifero(animal):
     def __init__(self, tipo, mamifero):
         super().__init__(tipo)
+
         self.mamifero=mamifero
 class oviparo(mamifero):
     def __init__(self, tipo, mamifero, oviparo):
         super().__init__(tipo, mamifero)
         self.oviparo=oviparo
+        
 pollo=animal("pollo")
 gato=mamifero("gato", True)
 ornitorrinco=oviparo("ornitorrinco", True, True)
