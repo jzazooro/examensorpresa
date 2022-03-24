cc4e9f7784aad58c4b5941656ecb61b9776664f2
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:10:44 +0100

    self

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,2 +1,7 @@
 class libro():
     def __init__(self, titulo, autor, editorial, isbn, fechadepublicacion):
+        self.titulo=titulo
+        self.autor=autor
+        self.editorial=editorial
+        self.isbn=isbn
+self.fechadepublicacion=fechadepublicacion
