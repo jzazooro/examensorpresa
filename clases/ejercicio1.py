a49bf034e237f063f497a3eadd591da4ba835e90
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:20:09 +0100

    correccion

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -5,7 +5,9 @@ class libro():
         self.editorial=editorial
         self.isbn=isbn
         self.fechadepublicacion=fechadepublicacion
+
     def libro(self):
         print("se trata del libro", self.titulo, "del autor", self.autor, "de la editorial", self.editorial, "con isbn", self.isbn, "que se publico el dia", self.fechadepublicacion)
+        
 busqueda=libro("cancion de hielo y fuego", "George R R Martin", "Gigamesh", "F4J7VN49", "1 de agosto de 1996")
 busqueda.libro()
