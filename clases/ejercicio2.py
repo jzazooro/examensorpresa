658fd9295c3eb2cb16e048b32e2cef83dd7a11af
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:40:41 +0100

    clase mamifero

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -3,6 +3,7 @@ class animal(self, tipo):
         self.tipo=tipo
 class mamifero(animal):
     def __init__(self, tipo, mamifero):
-
+        super().__init__(tipo)
+        self.mamifero=mamifero
 class oviparo(mamifero):
     def __init__(self, tipo, mamifero, oviparo):
