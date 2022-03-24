f88e8ff004e0eacf75d6944261faf4da2d84de32
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 10:21:38 +0100

    ingresar dinero

--- clases/ejercicio4.py
+++ clases/ejercicio4.py
@@ -10,9 +10,18 @@ class cuentabancaria():
         retiro=int(input("¿cuanto dinero desea retirar?: "))
         if retiro > saldo:
             saldo = saldo
-            print("no se puede realizar esta operacion ya que no dispone del dinero suficiente, su saldo actual es", saldo)
+            print("no se puede realizar esta operacion ya que no dispone del dinero suficiente, su saldo actual es: ", saldo)
         if retiro >= saldo:
             saldo=saldo-retiro
             print("la operacion de ha realizado con exito, su saldo es", saldo) 
+    def ingresardinero(saldo):
+        ingreso=int(input("¿cuanto dinero desea ingresar?: "))
+        if ingreso <= 0:
+            saldo=saldo
+            print("no se puede realizar esta operacion, su saldo es: ", saldo)
+        if ingreso > 0:
+            saldo=saldo+ingreso
+            print("la operacion se ha realizado con exito, su saldo es: ", saldo)
+
 
     
