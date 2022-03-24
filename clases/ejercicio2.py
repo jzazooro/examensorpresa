d0006be36065207026ca97d20bb561efcbc0a0c5
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:53:46 +0100

    ejemplo del ornitorrinco

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -9,3 +9,7 @@ class oviparo(mamifero):
     def __init__(self, tipo, mamifero, oviparo):
         super().__init__(tipo, mamifero)
         self.oviparo=oviparo
+pollo=animal("pollo")
+gato=mamifero("gato", True)
+ornitorrinco=oviparo("ornitorrinco", True, True)
+print(ornitorrinco.mamifero)
