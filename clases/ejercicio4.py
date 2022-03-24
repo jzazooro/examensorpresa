5530d46d66235cbc80b6314f7b52b58b2881ed79
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 10:17:19 +0100

    retirar dinero

--- clases/ejercicio4.py
+++ clases/ejercicio4.py
@@ -5,4 +5,14 @@ class cuentabancaria():
         self.fechadeapertura=fechadeapertura
         self.numerodecuenta=numerodecuenta
         self.saldo=saldo
+
+    def retirardinero(saldo):
+        retiro=int(input("¿cuanto dinero desea retirar?: "))
+        if retiro > saldo:
+            saldo = saldo
+            print("no se puede realizar esta operacion ya que no dispone del dinero suficiente, su saldo actual es", saldo)
+        if retiro >= saldo:
+            saldo=saldo-retiro
+            print("la operacion de ha realizado con exito, su saldo es", saldo) 
+
     
