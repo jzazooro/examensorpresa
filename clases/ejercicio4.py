3b42b86d4330f2b63f2050b940a9b0acca161a3a
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 10:09:41 +0100

    definimos la cuenta bancaria

---
+++ clases/ejercicio4.py
@@ -0,0 +1,8 @@
+class cuentabancaria():
+    def __init__(self, id, nombredeltitular, fechadeapertura, numerodecuenta, saldo):
+        self.id=id
+        self.nombredeltitular=nombredeltitular
+        self.fechadeapertura=fechadeapertura
+        self.numerodecuenta=numerodecuenta
+        self.saldo=saldo
+    
