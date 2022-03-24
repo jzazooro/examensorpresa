dee4a95e7996d64df04c02a494241c8f9b7240a9
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-03-24 09:38:30 +0100

    definimos los __init__

--- clases/ejercicio2.py
+++ clases/ejercicio2.py
@@ -2,5 +2,7 @@ class animal(self, tipo):
     def __init__(self, tipo):
         self.tipo=tipo
 class mamifero(animal):
+    def __init__(self, tipo, mamifero):
 
 class oviparo(mamifero):
+    def __init__(self, tipo, mamifero, oviparo):
